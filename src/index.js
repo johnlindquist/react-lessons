00db559a2351a21a271cbb7044072edc5a97787c
John Lindquist <johnlindquist@gmail.com>
2018-08-10 11:40:50 -0600

    spread

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react"
+import { render } from "react-dom"
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Demo extends React.Component {
+  render() {
+    return (
+      <h2>
+        {this.props.greeting}, {this.props.name}
+      </h2>
+    )
+  }
+}
+
+const greeting = "hello"
+const name = "John"
+
+const App = () => <Demo {...{ greeting, name }} />
+
+render(<App />, document.getElementById("root"))
